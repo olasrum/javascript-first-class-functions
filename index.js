5c420e5a159b50513e08152e2100b0b10b1975a0
olasrum <mursalo@gmail.com>
2022-07-26 10:31:01 +1000

    Done with assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+function Monday() {
+    console.log('Go for a five-mile run')
+    console.log('Pump iron')
+}
+
+function Tuesday() {
+    console.log('Go for a five-mile run');
+    console.log('Swim 40 laps');
+}
+  
+function Wednesday() {
+    console.log('Go for a five-mile run');
+    console.log('Go for a five-mile run');
+}
+  
+function Thursday() {
+    console.log('Go for a five-mile run');
+    console.log('Pump iron');
+  }
+  
+function Friday() {
+    console.log('Go for a five-mile run');
+    console.log('Swim 40 laps');
+}
+
+function runFiveMiles() {
+    console.log('Go for a five-mile run');
+}
+
+function liftWeights() {
+    console.log('Pump iron');
+}
+
+  liftWeights()
